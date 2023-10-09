abc4916d5a5f7fdad1d3a2a0aaff721206c482b6
¨Ruben¨ <ruben.baronetto@darwoft.com>
2023-10-09 12:11:01 -0300

    Added initialization of appium services by code

--- Basic/LaunchAppTest.py
+++ Basic/LaunchAppTest.py
@@ -2,6 +2,11 @@ from appium import webdriver
 from typing import Any, Dict
 from appium.options.common import AppiumOptions
 from appium.webdriver.common.appiumby import AppiumBy
+from appium.webdriver.appium_service import AppiumService
+
+appium_service = AppiumService()
+
+appium_service.start()
 
 cap: Dict[str, Any] = {
     'platformName': 'Android',
@@ -15,9 +20,11 @@ cap: Dict[str, Any] = {
     'autoGrantPermissions': 'true'
 }
 
-url = 'http://127.0.0.1:4723/wd/hub'
+url = 'http://127.0.0.1:4723'
 driver = webdriver.Remote(url, options=AppiumOptions().load_capabilities(cap))
 
 ele_id = driver.find_element(AppiumBy.XPATH, '//android.widget.Button[@content-desc="Btn2"]')
 ele_id.click()
 driver.quit()
+
+appium_service.stop()
