f89bf97008f51bfd1ff82b50ddc6bbb8f8d42c7c
¨Ruben¨ <ruben.baronetto@darwoft.com>
2023-10-29 19:28:49 -0300

    Element actions

--- Element_actions/Element_actions.py
+++ Element_actions/Element_actions.py
@@ -1,8 +1,11 @@
+import time
+
 from appium import webdriver
 from typing import Any, Dict
 from appium.options.common import AppiumOptions
 from appium.webdriver.common.appiumby import AppiumBy
-
+from selenium.common import ElementNotVisibleException, ElementNotSelectableException, NoSuchElementException
+from selenium.webdriver.support.wait import WebDriverWait
 
 cap: Dict[str, Any] = {
     'platformName': 'Android',
@@ -18,6 +21,17 @@ cap: Dict[str, Any] = {
 
 url = 'http://127.0.0.1:4723/wd/hub'
 driver = webdriver.Remote(url, options=AppiumOptions().load_capabilities(cap))
-
+wait = WebDriverWait(driver, 25, 1, ignored_exceptions=[ElementNotVisibleException, ElementNotSelectableException,
+                                                        NoSuchElementException])
 ele = driver.find_element(AppiumBy.CLASS_NAME, 'android.widget.Button')
+
 print("Text on the button: ", ele.text)
+print("Text on the button: ", ele.get_attribute("text"))
+print("Text on the button: ", ele.get_attribute("content-desc"))
+
+ele.click()
+
+eleInput = wait.until(lambda x: x.find_element(AppiumBy.CLASS_NAME, 'android.widget.EditText'))
+
+eleInput.send_keys("test")
+eleInput.clear()
