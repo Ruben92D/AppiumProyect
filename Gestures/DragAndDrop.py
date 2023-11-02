17fd9f2059754912b699d531592876690d907b48
¨Ruben¨ <ruben.baronetto@darwoft.com>
2023-11-02 12:54:16 -0300

    drag and drop

--- Gestures/DragAndDrop.py
+++ Gestures/DragAndDrop.py
@@ -9,10 +9,6 @@ from appium.webdriver.common.touch_action import TouchAction
 from selenium.common import ElementNotVisibleException, ElementNotSelectableException, NoSuchElementException
 from selenium.webdriver.support.wait import WebDriverWait
 
-# appium_service = AppiumService()
-#
-# appium_service.start()
-
 cap: Dict[str, Any] = {
     'platformName': 'Android',
     'platformVersion': '13',
@@ -28,27 +24,13 @@ cap: Dict[str, Any] = {
 url = 'http://127.0.0.1:4723/wd/hub'
 driver = webdriver.Remote(url, options=AppiumOptions().load_capabilities(cap))
 wait = WebDriverWait(driver, 25, 1, [ElementNotVisibleException, ElementNotSelectableException, NoSuchElementException])
-# scrollBtn = driver.find_element(AppiumBy.ID, 'com.code2lead.kwad:id/ScrollView')
-# scrollBtn.click()
-
-# Scroll into view btnSixteen = wait.until(lambda x: x.find_element(AppiumBy.ANDROID_UIAUTOMATOR,'new UiScrollable(
-# new UiSelector()).scrollIntoView(text("BUTTON16"))'))
-#
-# btnSixteen.click()
-#
-# yespopupbtn = wait.until(lambda x:  x.find_element(AppiumBy.ID, 'android:id/button1'))
-#
-# yespopupbtn.click()
-# long click
-
 actions = TouchAction(driver)
-# longClikBtn = wait.until(lambda x: x.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiScrollable(UiSelector(
-# ).text("LONG CLICK"))')) actions.long_press(longClikBtn,5) actions.perform()
 
+element = wait.until(lambda x: x.find_element(AppiumBy.ANDROID_UIAUTOMATOR,
+                                              'new UiScrollable(new UiSelector()).scrollIntoView(text("DRAGANDDROP"))'))
+element.click()
 
-# tap elle = wait.until(lambda x: x.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiScrollable(UiSelector().text(
-# "LOGIN"))')) elle = driver.find_element(AppiumBy.ID, "com.code2lead.kwad:id/Zoom") actions.tap(None, 700, 1273,
-# 1) actions.tap(elle) actions.perform()
+elementToDrag = wait.until(lambda x: x.find_element(AppiumBy.ID, 'com.code2lead.kwad:id/ingvw'))
+elementToDrop = wait.until(lambda x: x.find_element(AppiumBy.ID, 'com.code2lead.kwad:id/layout2'))
 
-
-# appium_service.stop()
+actions.long_press(elementToDrag).move_to(elementToDrop).release().perform()
